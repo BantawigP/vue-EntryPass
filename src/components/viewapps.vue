f0d41e2930e4e843704ce707c2146334e0879e16
BantawigP <142190056+BantawigP@users.noreply.github.com>
2023-12-14 22:35:57 +0800

    ssd

--- src/components/viewapps.vue
+++ src/components/viewapps.vue
@@ -1,8 +1,6 @@
 <script setup>
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
 import { ref } from 'vue';
 
 const categories = ref([
@@ -23,47 +21,6 @@ const categories = ref([
   }
 ]);
 
-const editingCategory = ref(null);
-const newCategory = ref({ date: '', time: '', office: ''}); // New category object
-
-const editCategory = (category) => {
-  editingCategory.value = { ...category.data }; // Set editingCategory to the selected category's data
-};
-
-const saveEditedCategory = () => {
-  if (editingCategory.value) {
-    // Update the category data with the edited values
-    const index = categories.value.findIndex((c) => c.id === editingCategory.value.id);
-    categories.value[index].date = editingCategory.value.date;
-    categories.value[index].time = editingCategory.value.time;
-    categories.value[index].office = editingCategory.value.office;
-    editingCategory.value = null;
-  }
-};
-
-const cancelEdit = () => {
-  editingCategory.value = null;
-};
-
-const deleteCategory = (category) => {
-  // Find the index of the category
-  const index = categories.value.findIndex((c) => c.id === category.id);
-  // Remove the category from the categories array
-  categories.value.splice(index, 1);
-};
-
-const addCategory = () => {
-  if (newCategory.value.name && newCategory.value.description) {
-    // Generate a new unique ID (replace with your actual ID generation logic)
-    const newId = Math.max(...categories.value.map((c) => c.id)) + 1;
-    newCategory.value.id = newId;
-    categories.value.push({ ...newCategory.value });
-    // Clear the form for the next addition
-    newCategory.value.date = '';
-    newCategory.value.time = '';
-    newCategory.value.office = '';
-  }
-};
 </script>
 
 <template>
@@ -75,8 +32,7 @@ const addCategory = () => {
       <Column field="date" header="Date"></Column>
       <Column field="time" header="Time"></Column>
       <Column field="office" header="Office"></Column>
-    
-       </DataTable>
+    </DataTable>
   </div>
 </body>
 </template>
