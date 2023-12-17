750bb365d13115c835128d679394a11108ae091c
RJ <rjpaasa@gmail.com>
2023-12-17 22:29:48 +0800

    Finish Guard and Booking

--- src/components/guard.vue
+++ src/components/guard.vue
@@ -5,7 +5,6 @@ import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import { ref } from 'vue';
 
-
 const categories = ref([
   {
     id: 1,
@@ -71,8 +70,36 @@ const addCategory = () => {
 
 <template>
   <body>
+    
+    <div class="Whitebackground"><form class="logos">
+  <img src="/src/assets/uic1.png" class="uiclogo">
+  <img src="/src/assets/myuic.png" class="myuic">
+</form>
+    
+    <div class="Graybackground"></div>
+    
+    <div class="Curvebox">
+      <h2 class="Admin">Admin</h2>
+    </div>
+    <div class="Curvebox1">
+      <h2 class="OIC">Office in Charge</h2>
+    </div>
+    <div class="Curvebox2">
+      <h2 class="Guard">Guard</h2>
+    </div>
+    <div class="Curvebox3">
+      <h2 class="Booking">Booking</h2>
+    </div>
+    <div class="Pinkbackground">
+      <h1>Visitor Information</h1>
+      
+    <div class="Whitebackground1">
+      
+
+  
   <div>
-    <h1>Visitor Information</h1>
+    
+    
     <DataTable :value="categories" tableStyle="min-width: 50rem">
       <Column field="id" header="ID"></Column>
       <Column field="name" header="Name"></Column>
@@ -117,22 +144,22 @@ const addCategory = () => {
 
     <!-- Add Category Form -->
     <div>
-      <h2>Add Visitor</h2>
+      <h2 class="Addvisitor">Add Visitor</h2>
       <form @submit.prevent="addCategory">
         <div>
-          <label for="newName">Name:</label>
+          <label class="Name" for="newName">Name:</label>
           <InputText id="newName" v-model="newCategory.name" />
         </div>
         <div>
-          <label for="newDate">Date:</label>
+          <label class="Date" for="newDate">Date:</label>
           <InputText id="newDate" v-model="newCategory.date" />
         </div> 
         <div>
-          <label for="newTime">Time:</label>
+          <label class="Time" for="newTime">Time:</label>
           <InputText id="newTime" v-model="newCategory.time" />
         </div>
         <div>
-          <label for="newOffice">Office:</label>
+          <label class="Office" for="newOffice">Office:</label>
           <InputText id="newOffice" v-model="newCategory.office" />
         </div> 
         <div>
@@ -141,6 +168,9 @@ const addCategory = () => {
       </form>
     </div>
   </div>
+</div>
+</div>
+</div>
 </body>
 </template>
 
@@ -152,5 +182,217 @@ width: 1920px;
 height: 1080px;
 left: 0px;
 top: 0px;
+}
+.Whitebackground{
+  position: relative;
+  border: 1px solid;
+  width: 1683px;
+  height: 902px;
+  top: 12%;
+  padding: 2% 0 0;
+  margin: auto;
+  background-color:rgba(217, 217, 217, 85%);
+}
+
+.Whitebackground1{
+  position: absolute;
+  border: 1px solid;
+  width: 1045px;
+  height: 496px;
+  right: 10%;
+  bottom: 8%;
+  background-color:rgba(217, 217, 217, 50%);
+}
+.myuic{
+position: absolute;
+margin: auto;
+width:222px;
+height:90px;
+top: 9.5%;
+right: 39.5%;
+display: block;
+
+}
+.uiclogo{
+position: absolute;
+margin:auto;
+width:215px;
+height:215px;
+top: -12.5%;
+right: 40%;
+display: block;
+padding-bottom: 8px;
+}
+
+.p-button-primary{
+  position: absolute;
+  right: 71%;
+  bottom: 2%;
+  font-family: arial;
+}
+
+.Addvisitor{
+  position: absolute;
+  right: 69%;
+  bottom: 40%;
+}
+
+.Name{
+  position: absolute;
+  left: 7%;
+  bottom: 37%;
+}
+
+
+#newName{
+  position: absolute;
+  border-radius: 10px;
+  width: 269px;
+  height: 35px;
+  top: 58%;
+  left: 12%;
+}
+
+.Date{
+  position: absolute;
+  left: 7%;
+  bottom: 28.5%;
+}
+
+#newDate{
+  position: absolute;
+  border-radius: 10px;
+  width: 269px;
+  height: 35px;
+  top: 66%;
+  left: 12%;
+}
+
+.Time{
+  position: absolute;
+  left: 7%;
+  bottom: 20.5%;
+}
+
+#newTime{
+  position: absolute;
+  border-radius: 10px;
+  width: 269px;
+  height: 35px;
+  top: 74%;
+  left: 12%;
+}
+
+.Office{
+  position: absolute;
+  left: 7%;
+  bottom: 12.5%;
+}
+
+#newOffice{
+  position: absolute;
+  border-radius: 10px;
+  width: 269px;
+  height: 35px;
+  top: 82.2%;
+  left: 12%;
+}
+
+
+h1{
+  position: absolute;
+  right: 37.5%;
+  top: 1%;
+  font-family: arial;
+}
+
+.Admin{
+  position: absolute;
+  right: 32%;
+  bottom: -28%;
+  font-family: arial;
+}
+
+.OIC{
+  position: absolute;
+  width: 190px;
+  right: 4%;
+  bottom: -30%;
+  font-family: arial;
+}
+
+.Guard{
+  position: absolute;
+  right: 34%;
+  bottom: -20%;
+  font-family: arial;
+}
+
+.Booking{
+  position: absolute;
+  right: 26%;
+  bottom: -28%;
+  font-family: arial;
+}
+
+.Pinkbackground{
+  position: absolute;
+  border: 1px solid;
+  width: 1319px;
+  height: 641px;
+  top: 20%;
+  right: 2%;
+  padding: 2% 0 0;
+  margin: auto;
+  background-color:rgba(234, 198, 235, 100%);
+}
+.Graybackground{
+  position: absolute;
+  border-radius: 20px;
+  top: 25%;
+  left: 2%;
+  width: 271px;
+  height: 576px;
+  background-color:rgba(184, 184, 184, 100%);
+}
+
+.Curvebox{
+  position: absolute;
+  border-radius: 15px;
+  width: 210px;
+  height: 43px;
+  top: 28%;
+  left: 3.8%;
+  background-color:rgba(255, 255, 255, 100%);
+}
+
+.Curvebox1{
+  position: absolute;
+  border-radius: 15px;
+  width: 210px;
+  height: 43px;
+  top: 35%;
+  left: 3.8%;
+  background-color:rgba(255, 255, 255, 100%);
+}
+
+.Curvebox2{
+  position: absolute;
+  border-radius: 15px;
+  width: 234px;
+  height: 51px;
+  top: 42%;
+  left: 3%;
+  background-color:rgba(238, 139, 204, 100%);
+}
+
+.Curvebox3{
+  position: absolute;
+  border-radius: 15px;
+  width: 210px;
+  height: 43px;
+  top: 50%;
+  left: 3.8%;
+  background-color:rgba(255, 255, 255, 100%);
 }
 </style>
