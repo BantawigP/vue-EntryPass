d57a3b245e8d279fa72f066edf632f6141835a3f
BantawigP <142190056+BantawigP@users.noreply.github.com>
2024-04-16 00:27:19 +0800

    partial done booking

--- src/components/booking.vue
+++ src/components/booking.vue
@@ -1,146 +1,182 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 
+
+const validate = () => {
+            const Fname = document.getElementById('fname').value;
+            const Lname = document.getElementById('lname').value;
+            const date = document.getElementById('date').value;
+            const time = document.getElementById('time').value;
+            const pov = document.getElementById('pov').value;
+            
+
+            if (Fname === '' && Lname === '' && date ==='' && time ==='' && pov ==='') {
+                alert('Please fill up the form to proceed');
+                
+            }
+            else if(username === 'cashier' && password === 'password'){
+              window.open('/cashierOffice','_self');
+            }
+            else if(username === 'visitor' && password === 'password'){
+              window.open('/home','_self');
+            } 
+            else {
+                alert('Invalid username or password. Please try again.');
+            }
+        };
 </script>
 
 <template>
   <body>
- <div class="frame">
-  <div class="sidepanel">
-  <aside>
-    <div class="wrapper">
-    <div class="image_logo">
-          <img class="logo" src="/src/assets/logo.enp.png">
+    <div class="frame">
+      <div class="sidepanel">
+        <aside>
+          <div class="wrapper">
+            <div class="image_logo">
+              <img class="logo" src="/src/assets/logo.enp.png">
+            </div>
+            <div class="header_title">
+              <p>Automated Entry Pass</p>
+            </div>  
+          </div>
+          <nav class="nav flex-column">
+            <p class="welcomMess">Hi Visitors!</p>
+            <a class="nav-link-book" href="#">Book Appointment</a>
+            <a class="nav-link-view" href="/viewapps">View Appointment</a>
+            <a class="nav-link-settings" href="#">Settings</a>
+            <a class="nav-link-logout" href="/">Logout</a>
+          </nav>
+        </aside>
+      </div>
+      <div id="mainframe">
+        <div id="frame1">
+          <form>
+
+              <input class="fname" id="fname" v-model="value1" type="text" placeholder="First Name"/>
+              <input class="lname" id="lname" v-model="value2" type="text" placeholder="Last Name"/>
+              <input class="date" id="date" v-model="value3" type="text" placeholder="MM/DD/YYYY"/>
+              <input class="time" id="time" v-model="value4" type="text" placeholder="12:00"/>
+              <input class="pov" id="pov" v-model="value5" type="text" placeholder="Purpose of Visit"/>
+
+              <Button @click="validate()">Submit</Button>
+            
+          </form>
         </div>
-     <div class="header_title">
-      <p>Automated Entry Pass</p>
-     </div>  
+      </div>
     </div>
-    <nav class="nav flex-column">
-      <p class="welcomMess">Hi Visitors!</p>
-  <a class="nav-link-book" href="#">Book Appointment</a>
-  <a class="nav-link-view" href="/viewapps">View Appointment</a>
-  <a class="nav-link-settings" href="#">Settings</a>
-  <a class="nav-link-logout" href="/">Logout</a>
-</nav>
-  </aside>
-</div>
- <div id="mainframe">
-   <div id="frame1">
-    <div id="content" style="z-index: 1;">
-      <InputText class="user" id="fname" v-model="value1" type="text" size="small" placeholder="First Name"/>
-      <InputText class="user" id="lname" v-model="value2" type="text" size="small" placeholder="Last Name"/>
-      <InputText class="user" id="date" v-model="value3" type="text" size="small" placeholder="<MM/DD/YYYY"/>
-      <InputText class="user" id="time" v-model="value4" type="text" size="small" placeholder="12:00"/>
-      <InputText class="user" id="pov" v-model="value5" type="text" size="small" placeholder="Purpose of Visit"/>
-    </div>
-
-  </div>
-
-  </div>
- </div>
-
-</body>
+  </body>
 </template>
 
 <style scoped>
-.frame{
-  width: 1528px;
-  height: 755px;
-  position:relative;
-  background-image: url('/src/assets/bg.png');
+  .frame{
+    width: 1528px;
+    height: 755px;
+    display: flex;
+    background-image: url('/src/assets/bg.png');
   background-size:cover;
   background-repeat: no-repeat;
-  display:flex;
-}
-#content{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 1; 
+
   }
-p{
+
+  .sidepanel{
+    width: 288.84px;
+    height: 755px;
+    background-color: #F27B8A;
+  }
+
+  .wrapper{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+  }
+
+  .header_title{
+    text-align: left;
+    margin-left: 20px;
+    margin-top: 10px;
+  }
+
+  .logo{
+    width: 40px;
+    height: 35px;
+    border-radius: 20px;
+  }
+
+  .nav {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+  }
+  
+  .nav-link-book{
   color: rgb(255, 255, 255);
-}
-#mainframe{
-  background-color: rgba(238, 139, 204, 0.24); 
-  width: calc(100% - 288.84px); 
-  height: 100%; 
-  float: right;  
-  top: 0; 
-  right: 0;
-  flex:1; 
-  position: relative;
-}
-#frame1{
- 
-  float: center;
-  width: 50%;
-  padding: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: rgba(255, 255, 255, 1);
-  z-index: 0;
-}
-aside{
-float: left;
-background-color: #F27B8A;
-width: 288.84px;
-height: 755px;
-}
-.logo{
-  width: 39.79px;
-  height: 35.01px;
-  position:static;
-  border-radius: 20px;
-  margin-top: 37px;
-  margin-left: 23px;
-}
-.nav-link-book{
-  color: rgb(0, 0, 0);
   text-decoration: none;
   margin-left: 23px;
   margin-bottom:24px;
   margin-top: 70px;
 }
+
 .nav-link-view{
   color: rgb(255, 255, 255);
   text-decoration: none;
   margin-left: 23px;
   margin-bottom: 319px;
 }
+
 .nav-link-settings{
   color: rgb(255, 255, 255);
   text-decoration: none;
   margin-left: 23px;
   margin-bottom: 18px;
 }
+
 .nav-link-logout{
   color: rgb(255, 255, 255);
   text-decoration: none;
   margin-left: 23px;
 }
-.image_logo{
-  flex:1;
-  margin-right: 10px;
+
+  .welcomMess{
+    margin-left: 20px;
+    margin-top: 20px;
+  }
+
+  #mainframe{
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    background-color: rgba(238, 139, 204, 0.24); 
+  }
+
+  #frame1{
+    width: 50%;
+    background-color: #f2f2f2;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+
+  button{
+display: block;
+position: relative;
+margin:auto;
+background-color: #FA7B9F;
+border-radius: 20px;
+border-bottom: 10px;
+height: 44px;
+width: 180px;
 }
-.wrapper{
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.header_title{
-  flex: 2;
-  text-align: left;
-  margin-top: 55px;
-  padding-right: 45px;
-  
-}
-.welcomMess{
-    margin-left: 23px;
-    margin-top: 44px;
-}
+
+  input[type="text"] {
+    width: calc(100% - 12px);
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+
 
 </style>
